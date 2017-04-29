6d379dd5b8ebccd721f00fa31bad37eec4a89efc
alex <wertycal@yandex.ru>
2017-04-29 10:54:39 +0400

    Заготовка для формы

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/">
+</head>
+<body>
+
+<div class="form">
+	
+	<form>
+		<input type="text" name="task-name">
+		<input type="submit" name="add-task" value="Добавить">
+	</form>
+
+</div>
+
+</body>
+</html>
